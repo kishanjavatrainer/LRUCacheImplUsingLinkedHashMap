84167f79f5108e9181374c8efb3e8a5c8226f56e
kishanjavatrainer <kishan.javatrainer@gmail.com>
2018-03-14 19:03:27 +0530

    Update LRUCache.java

--- LRUCacheImplUsingLinkedHashMap/src/com/infotech/cache/LRUCache.java
+++ LRUCacheImplUsingLinkedHashMap/src/com/infotech/cache/LRUCache.java
@@ -4,18 +4,9 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+	
 	private static final long serialVersionUID = -4622177786359540728L;
 	private int size;
-
-    public static void main(String[] args) {
-        LRUCache<Integer, Integer> cache = LRUCache.newInstance(2);
-        cache.put(1, 1);
-        cache.put(2, 2);
-        cache.put(1, 1);
-        cache.put(3, 3);
-
-        System.out.println(cache);
-    }
 
     private LRUCache(int size) {
         super(size, 0.75f, true);
